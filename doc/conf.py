9654fffc33e8837b9d498dfd3dc482e10f06bffa
Simona Maggio <maggio.simona@gmail.com>
2020-11-10 21:00:17 +0100

    rm google analytics id

--- doc/conf.py
+++ doc/conf.py
@@ -142,9 +142,7 @@ def setup(app):
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    'analytics_id': 'UA-169600606-1'
-}
+#html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
