af938941753b6f6f0d345a753ecfc72cdfabb140
Simona Maggio <maggio.simona@gmail.com>
2020-11-12 10:23:18 +0100

    should be a ceil, not round

--- mea/error_analyzer.py
+++ mea/error_analyzer.py
@@ -413,8 +413,8 @@ class ErrorAnalyzer(object):
         leaves_summary = []
         for leaf_id in leaf_nodes:
             values = self.model_performance_predictor.tree_.value[leaf_id, :]
-            n_errors = int(round(values[0, error_class_idx], 2))
-            n_corrects = int(round(values[0, correct_class_idx], 2))
+            n_errors = np.ceil(values[0, error_class_idx])
+            n_corrects = np.ceil(values[0, correct_class_idx])
             local_error = float(n_errors) / (n_corrects + n_errors)
             global_error = float(n_errors) / n_total_errors
 
