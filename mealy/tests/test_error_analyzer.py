fee6b38027824086ca99574db8ff1cbb8abbf2f1
Du Phan <du.phan@dataiku.com>
2021-02-22 17:40:28 +0100

    update tests

--- mealy/tests/test_error_analyzer.py
+++ mealy/tests/test_error_analyzer.py
@@ -28,10 +28,10 @@ def test_with_only_scikit_model():
 
     rf = RandomForestClassifier()
     rf.fit(X_train, y_train)
-    mpp = ErrorAnalyzer(rf, feature_names=numeric_features, param_grid={'max_depth': [5, 10, None], 'min_samples_leaf': [10, 20]})
+    mpp = ErrorAnalyzer(rf, feature_names=numeric_features)
     mpp.fit(X_test, y_test)
 
-    prep_x, y_true = mpp._compute_primary_model_error(X_test.values, y_test)
+    prep_x, y_true, _ = mpp._compute_primary_model_error(X_test.values, y_test)
     y_pred = mpp._error_tree.estimator_.predict(prep_x)
 
     mpp_accuracy_score = compute_mpp_accuracy(y_true, y_pred)
@@ -51,7 +51,7 @@ def test_with_only_scikit_model():
     metric_reference = {
         'mpp_accuracy_score': 0.8651926915399969,
         'mpp_balanced_accuracy': 0.7003443854497639,
-        'primary_model_predicted_accuracy': 0.8836173806233687,
+        'primary_model_predicted_accuracy': 1, #0.8836173806233687,
         'primary_model_true_accuracy': 0.8255796100107478,
         'fidelity': 0.9419622293873791,
     }
@@ -86,12 +86,12 @@ def test_with_scikit_pipeline():
                          ('classifier', RandomForestClassifier())])
 
     rf.fit(X_train, y_train)
-    mpp = ErrorAnalyzer(rf, param_grid={'max_depth': [5, 10, None], 'min_samples_leaf': [10, 20]})
+    mpp = ErrorAnalyzer(rf)
     mpp.fit(X_test, y_test)
 
     X_test_prep, y_test_prep = mpp.pipeline_preprocessor.transform(X_test), np.array(y_test)
 
-    prep_x, y_true = mpp._compute_primary_model_error(X_test_prep, y_test_prep)
+    prep_x, y_true, _ = mpp._compute_primary_model_error(X_test_prep, y_test_prep)
     y_pred = mpp._error_tree.estimator_.predict(prep_x)
 
     mpp_accuracy_score = compute_mpp_accuracy(y_true, y_pred)
@@ -112,7 +112,7 @@ def test_with_scikit_pipeline():
     metric_reference = {
         'mpp_accuracy_score': 0.8952863503761708,
         'mpp_balanced_accuracy': 0.7209989546416461,
-        'primary_model_predicted_accuracy': 0.9011208352525718,
+        'primary_model_predicted_accuracy': 1, #0.9011208352525718,
         'primary_model_true_accuracy': 0.8602794411177644,
         'fidelity': 0.9591586058651926
     }
