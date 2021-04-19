d5c27e5e564b16f59b8877786f895e2b4e597c17
Du Phan <du.phan@dataiku.com>
2021-04-19 17:48:00 +0200

    add ErrorAnalyzerConstant in __init__

--- mealy/__init__.py
+++ mealy/__init__.py
@@ -1,4 +1,5 @@
 # Dataiku Model Error Analysis lib
 from .version import __version__
+from mealy.constants import ErrorAnalyzerConstants
 from mealy.error_analyzer import ErrorAnalyzer
 from mealy.error_visualizer import ErrorVisualizer, _BaseErrorVisualizer
